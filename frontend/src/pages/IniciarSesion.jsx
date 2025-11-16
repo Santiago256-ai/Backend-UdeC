a4a64bf331baf8f4ecf646cb713e224afdfd68ed
Santiago256-ai <olivarrodriguez25@gmail.com>
2025-11-16 18:19:18 -0500

    Subiendo backend inicial

--- frontend/src/pages/IniciarSesion.jsx
+++
@@ -1,8 +0,0 @@
-// IniciarSesion.jsx
-export default function Login() {
-  return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-2xl font-bold">Iniciar Sesión</h1>
-    </div>
-  );
-}

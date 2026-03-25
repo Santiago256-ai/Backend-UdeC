6953f72fa1abce32fc563f2bac967f63f2e1d0c0
Santiago256-ai <olivarrodriguez25@gmail.com>
2026-03-25 18:53:50 -0500

    fix: Agregar modalidad a la bd2

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcryptjs'; 
-import jwt from 'jsonwebtoken'; // ⬅️ IMPORTAR JWT
+import jwt from 'jsonwebtoken'; 
 const prisma = new PrismaClient();
 
 // ⚠️ CLAVE SECRETA: Es crucial usar una clave compleja y leerla desde .env
